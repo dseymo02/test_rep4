c7d9c3dac28008eedd212369302d85c65ffd733b
dseymo02 <dseymo02@dcs.bbk.ac.uk>
2018-05-28 11:43:05 +0100

    more

--- question3/question3.py
+++ question3/question3.py
@@ -1,3 +1,15 @@
 def printwords(lst):
 	size = lst.pop(0)
-	return size
+	listorder = []
+	count = 0
+	distinct = []
+	for index in range(0,size):
+		if lst[index] not in distinct:
+			distinct.append(lst[index])
+	for word in distinct:
+		count = lst.count(word)
+		listorder.append((word,count))
+
+
+	return listorder, distinct
+
